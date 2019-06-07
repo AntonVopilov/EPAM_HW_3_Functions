32a9d9f62c6780dfb1495bd8ab96094b7855f480
AntonVopilov <50808154+AntonVopilov@users.noreply.github.com>
2019-06-07 17:18:25 +0300

    Add files via upload

--- hw_3.py
+++ hw_3.py
@@ -1,27 +1,24 @@
 
-counter_name = 0
 
 
-def make_it_count(func, counter_name):
 
-    def wrapper(*args):
-        global counter_name
-        counter_name += 1
-        return func(*args)
-    return wrapper
+def make_it_count(func, counter_name: str):
+    globals()[counter_name] += 1
+    return func
 
+another = 22
+new_f = make_it_count(lambda x: x**2, 'another')
 
-new_f = make_it_count(lambda x: x**2, counter_name)
 
 print(new_f)
 print(new_f(1))
-print(counter_name)
+print(another)
 
 print(new_f(2))
-print(counter_name)
+print(another)
 
 print(new_f(3))
-print(counter_name)
+print(another)
 
 print(new_f(50))
-print(counter_name)
+print(another)
